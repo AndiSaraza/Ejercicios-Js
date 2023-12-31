b58c8497a11261eff2784e27759d4bb8d5743af4
LandiSa <andreamsaraza@gmail.com>
2023-12-31 19:53:04 -0300

    se realiza el ejercicio 8

--- js/E8.js
+++ js/E8.js
@@ -2,3 +2,13 @@
 Ejemplo:  
 input: 10   input: 15
 Output: El 10 es divisible por 2.    Output: El 15 no es divisible por 2. */
+
+let n1=parseInt(prompt('Ingrese un numero'));
+let resultado= (n1%2);
+
+if (resultado==0) {
+  document.write('El numero ', n1,' es divisible por 2')  
+}else{
+    document.write('El numero ', n1,' no es divisible por 2')   
+}
+
